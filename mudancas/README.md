2f52e35cf8d41d2e6d7d0decab2cce363d8d8de4
Naíra de Moura e Souza <74928249+nairamouras@users.noreply.github.com>
2022-08-31 13:03:39 -0400

    Delete mudancas directory

--- mudancas/README.md
+++
@@ -1,2 +0,0 @@
-# instaview-raspberry
-Projeto de TV Instaview e Radio com Raspberry Pi para a disciplina de Sistemas Embarcados
--- mudancas/teste.py
+++
@@ -1,8 +0,0 @@
-
-import subprocess
-import vlc
-
-player = vlc.MediaPlayer('completo_vinheta.mp4')
-p = subprocess.Popen('cvlc', 'fullscreen', 'home/pi/demo.mp4', '--loop')
-player.play()
-

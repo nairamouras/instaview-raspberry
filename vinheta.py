b16548d057aee5778211912f0812b5d50917626a
Naíra de Moura e Souza <74928249+nairamouras@users.noreply.github.com>
2022-08-31 12:07:52 -0400

    Update vinheta.py

--- vinheta.py
+++ vinheta.py
@@ -2,23 +2,23 @@ from moviepy.editor import VideoFileClip, ColorClip, TextClip, CompositeVideoCli
 
 def vinheta():
     
-    video = VideoFileClip('./clips.mp4')
+    video = VideoFileClip('loop.mp4')
 
     f = open('noticias-ifmt.csv', 'r')
     data = f.read().replace('\n', '')
 
-    text = TextClip(data, color = 'green', fontsize = 80
+    text = TextClip(data, color = 'white',font = "Arial", fontsize = 80
         ).set_duration(video.duration)
 
     color = ColorClip(
-        text.size, color = (255, 255, 255), duration = video.duration
+        text.size, color = (0, 128, 0), duration = video.duration
     )
 
     cor_texto = CompositeVideoClip([color, text]).set_position(pos)
 
     compose = CompositeVideoClip([video, cor_texto])
 
-    compose.write_videofile('vinheta.mp4')
+    compose.write_videofile('completo_vinheta.mp4')
 
 def pos(time):
         return(time*-250, 'bottom')

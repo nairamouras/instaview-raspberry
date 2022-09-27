a2fe3f5bce324c20611d10fb539c26875505f194
Naíra de Moura e Souza <74928249+nairamouras@users.noreply.github.com>
2022-09-27 01:23:22 -0400

    Update pastaIFMT.py

--- pastaIFMT.py
+++ pastaIFMT.py
@@ -15,7 +15,6 @@ def correcaoPasta():
         if files[i].endswith('.mp4'):
             aux = os.path.splitext(files[i])
             auxi = []
-            print(aux)
             for j in range(tamanho):
                 if files[j].endswith('.jpg'):
                     auxi = os.path.splitext(files[j])
@@ -23,7 +22,6 @@ def correcaoPasta():
                     if auxi[0] == aux[0]:
                         lix = os.path.join(path, files[j])
                         if os.path.isfile(files[j]) == False:
-                            print(lix)
                             os.remove(lix)
                             aux = []
                     auxi = []
